f77aeca37a43f7f4d1f489672e498a0a66b8a60c
AlexanderK <73738345+AlexanderK666@users.noreply.github.com>
2022-12-17 13:02:58 +0300

    Init

---
+++ Qurre/Loader/EntryPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PluginAPI;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Loader;
+using PluginAPI.Loader.Features;
+
+namespace Qurre.Loader
+{
+    internal sealed class EntryPoint
+    {
+        public static EntryPoint Singleton { get; private set; }
+
+        [PluginConfig]
+        public MainConfig Config;
+
+        [PluginEntryPoint("Qurre", "2.0.0", "Framework for SCP:SL servers with unique functions & api.", "Qurre Team")]
+        private void LoadPlugin()
+        {
+            Singleton = this;
+
+        }
+    }
+}
---
+++ Qurre/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Qurre")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Qurre")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("64c5cf82-8b1a-4b79-94b5-dfe0f0e605b4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
