3361bd793e1ec06defd5e05d04a238a1b7bedecf
fydne <54745523+fydne@users.noreply.github.com>
2022-12-23 16:31:26 +0500

    Add enums of events

---
+++ Qurre/Events/EventTypes.cs
@@ -0,0 +1,21 @@
+﻿namespace Qurre.Events
+{
+    internal enum PlayerEvents : int
+    {
+        //100+ <- Network events
+        Joined = 101,
+        Left = 102,
+
+        //110+ <- Health events
+        Death = 111,
+
+        //120+ <- Admins with Player
+        Banned = 121,
+
+    }
+
+    internal enum MapEvents : int
+    {
+
+    }
+}
