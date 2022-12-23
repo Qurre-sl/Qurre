c3b29fa1a4768cd962c977f5d360a87fd9e772d5
fydne <54745523+fydne@users.noreply.github.com>
2022-12-23 16:41:22 +0500

    Update EventTypes.cs

--- Qurre/Events/EventTypes.cs
+++ Qurre/Events/EventTypes.cs
@@ -2,15 +2,35 @@
 {
     internal enum PlayerEvents : int
     {
-        //100+ <- Network events
-        Joined = 101,
-        Left = 102,
+        //1000+ <- Network events
+        Preauth = 1000,
+        Joined = 1001,
+        Left = 1002,
 
-        //110+ <- Health events
-        Death = 111,
+        //1100+ <- Health events
+        Death = 1101,
 
-        //120+ <- Admins with Player
-        Banned = 121,
+        //1200+ <- Admins with Player
+        Banned = 1201,
+        Kicked = 1202,
+
+        //1300+ <- Items
+        CancelUsingItem = 1301,
+        ChangeItem = 1302,
+        DropAmmo = 1303,
+        DropItem = 1304,
+        PickupAmmo = 1305,
+        PickupArmor = 1306,
+        PickupScp330 = 1307,
+        ChangeRadioRange = 1307,
+
+
+        //1400+ <- Interact
+        InteractGenerator = 1401,
+        InteractElevator = 1402,
+        InteractLocker = 1403,
+        InteractScp330 = 1404,
+        InteractShootingTarget = 1405,
 
     }
 
