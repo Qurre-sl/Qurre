22ae4f38e75c4c8aed0c89614f4d9c2f0bdf1c56
niruri23333 <xiaosuhihi@yeah.net>
2022-12-24 23:05:11 +0800

    alpha & round & map
    update

---
+++ Qurre/API/Controllers/Alpha.cs
@@ -0,0 +1,62 @@
+﻿using Mirror;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Qurre.API.Controllers
+{
+    public static class Alpha
+    {
+        public static AlphaWarheadController Controller { get; } = AlphaWarheadController.Singleton;
+        public static AlphaWarheadNukesitePanel InnerPanel => UnityEngine.Object.FindObjectOfType<AlphaWarheadNukesitePanel>();
+        public static AlphaWarheadOutsitePanel OutsidePanel => UnityEngine.Object.FindObjectOfType<AlphaWarheadOutsitePanel>();
+        public static GameObject InnerPanelever => InnerPanel.lever.gameObject;
+        public static bool InnerPaneleverStatus
+        {
+            get => InnerPanel.Networkenabled;
+            set
+            {
+                InnerPanel.Networkenabled = value;
+            }
+        }
+        public static bool Enabled
+        {
+            get => OutsidePanel.keycardEntered;
+            set => OutsidePanel.keycardEntered = value;
+        }
+        public static void Start(bool isAutomatic = true, bool suppressSubtitles = false)
+        {
+            Controller.InstantPrepare();
+            Controller.StartDetonation(isAutomatic, suppressSubtitles);
+        }
+        public static void InstantPrepare() => Controller.InstantPrepare();
+        public static void CancelDetonation() => Controller.CancelDetonation();
+        public static void Stop() => Controller.CancelDetonation();
+        public static void Detonate() => Controller.Detonate();
+        public static void Shake() => Controller.RpcShake(false);
+        public static bool Detonated { get; } = Controller._alreadyDetonated;
+        public static bool Active { get; } = Controller.Info.InProgress;
+        public static float TimeToDetonation
+        {
+            get => AlphaWarheadController.TimeUntilDetonation;
+            set
+            {
+                Controller.Info.StartTime = NetworkTime.time;
+                Controller.ForceTime(value);
+            }
+        }
+        public static bool Locked
+        {
+            get => Controller.IsLocked;
+            set => Controller.IsLocked = value;
+        }
+        public static int Cooldown
+        {
+            get => (int)Controller.CooldownEndTime;
+            set => Controller.NetworkCooldownEndTime = value;
+        }
+    }
+}
---
+++ Qurre/API/Round.cs
@@ -0,0 +1,99 @@
+﻿using GameCore;
+using Respawning;
+using RoundRestarting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static RoundSummary;
+using static ServerStatic;
+
+namespace Qurre.API
+{
+    public static class Round
+    {
+        internal static bool BotSpawned { get; set; } = false;
+        internal static bool ForceEnd { get; set; } = false;
+        public static TimeSpan ElapsedTime => RoundStart.RoundLength;
+        public static DateTime StartedTime => DateTime.Now - ElapsedTime;
+        public static int CurrentRound { get; internal set; } = 0;
+        public static int ActiveGenerators { get; internal set; } = 0;
+        public static float NextRespawn
+        {
+            get => RespawnManager.Singleton._timeForNextSequence - (float)RespawnManager.Singleton._stopwatch.Elapsed.TotalSeconds;
+            set => RespawnManager.Singleton._timeForNextSequence = value + (float)RespawnManager.Singleton._stopwatch.Elapsed.TotalSeconds;
+        }
+        public static bool Started { get; } = RoundSummary.RoundInProgress();
+        public static bool Ended { get; } = RoundSummary.singleton._roundEnded;
+        public static bool Waiting => RoundStart.singleton is not null && !Started && !Ended;
+        public static bool Lock
+        {
+            get => RoundSummary.RoundLock;
+            set => RoundSummary.RoundLock = value;
+        }
+        public static bool LobbyLock
+        {
+            get => RoundStart.LobbyLock;
+            set => RoundStart.LobbyLock = value;
+        }
+        public static int EscapedDPersonnel
+        {
+            get => RoundSummary.EscapedClassD;
+            set => RoundSummary.EscapedClassD = value;
+        }
+        public static int EscapedScientists
+        {
+            get => RoundSummary.EscapedScientists;
+            set => RoundSummary.EscapedScientists = value;
+        }
+        public static int ScpKills
+        {
+            get => RoundSummary.KilledBySCPs;
+            set => RoundSummary.KilledBySCPs = value;
+        }
+        public static int RoundKills
+        {
+            get => RoundSummary.Kills;
+            set => RoundSummary.Kills = value;
+        }
+        public static int ChangedZombies
+        {
+            get => RoundSummary.ChangedIntoZombies;
+            set => RoundSummary.ChangedIntoZombies = value;
+        }
+        public static void Restart(bool IsfastRestart = true, NextRoundAction action = NextRoundAction.DoNothing)
+        {
+            ServerStatic.StopNextRound = action;
+            bool oldfastRestartSetting = CustomNetworkManager.EnableFastRestart;
+            CustomNetworkManager.EnableFastRestart = IsfastRestart;
+            RoundRestart.InitiateRoundRestart();
+            CustomNetworkManager.EnableFastRestart = oldfastRestartSetting;
+        }
+        public static void Start() => CharacterClassManager.ForceRoundStart();
+        public static void End()
+        {
+            RoundSummary.singleton.ForceEnd();
+            ForceEnd = true;
+        }
+        public static void DimScreen() => RoundSummary.singleton.RpcDimScreen();
+        public static void ShowRoundSummary(RoundSummary.SumInfo_ClassList remainingPlayers, LeadingTeam team)
+        {
+            RoundSummary.singleton.RpcShowRoundSummary
+            (
+            listStart:RoundSummary.singleton.classlistStart,
+            listFinish: remainingPlayers,
+            leadingTeam: team,
+            eDS: EscapedDPersonnel,
+            eSc: EscapedScientists,
+            scpKills: ScpKills,
+            roundCd: Mathf.Clamp(ConfigFile.ServerConfig.GetInt("auto_round_restart_time", 10), 5, 1000),
+            seconds: 10
+            );
+        } 
+        public static void ForceTeamRespawn(bool isCI) => RespawnManager.Singleton.ForceSpawnTeam(isCI ? SpawnableTeamType.ChaosInsurgency : SpawnableTeamType.NineTailedFox);
+        public static void CallCICar() => RespawnEffectsController.ExecuteAllEffects(RespawnEffectsController.EffectType.Selection, SpawnableTeamType.ChaosInsurgency);
+        public static void CallMTFHelicopter() => RespawnEffectsController.ExecuteAllEffects(RespawnEffectsController.EffectType.Selection, SpawnableTeamType.NineTailedFox);
+    }
+}
