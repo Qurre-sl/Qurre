a18337d367fe343d8f8daf4609d57e7965e49b7c
Loutroop <3331802589@qq.com>
2022-12-27 19:46:33 +0800

    MovementStateUpdate

---
+++ Qurre/API/Classification/Player/MovementState.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qurre.API.Classification.Player
+{
+    using Qurre.API;
+    using Qurre.API.Addons;
+    using UnityEngine;
+
+    public class MovementState
+    {
+        private readonly Player _player;
+        internal MovementState(Player pl)
+        {
+            _player = pl;
+            
+        }
+        public Vector3 Postion
+        {
+            get => _player.GameObject.transform.position;
+            set => _player.GameObject.transform.position = value;  
+        }
+        public Quaternion Rotation
+        {
+            get => _player.GameObject.transform.rotation;
+            set => _player.GameObject.transform.rotation = value;
+        }
+        public Vector3 Scale
+        {
+            get => _player.GameObject.transform.localScale;
+            set => _player.GameObject.transform.localScale = value;
+        }
+    }
+}
